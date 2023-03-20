ed80207a6c52b4b557d26e4cd0d7df58d16a4e58
JuanOspina05 <juospinae@unal.edu.co>
2023-03-20 01:42:10 -0500

    fixed project

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class Usuario(AbstractUser):
+    nombre = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    celular = models.CharField(max_length=20, blank=True, null=True)
+    calificacion = models.FloatField(blank=True, null=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nombre']
+
+    def __str__(self):
+        return self.email
+    pass
