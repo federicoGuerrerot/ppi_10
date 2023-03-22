547a4f052e8fe3cd9f59bf5229e3042086c379e0
federicoGuerrerot <fguerrerot@unal.edu.co>
2023-03-21 22:16:28 -0500

    back inicial autenticacion usuario

--- tutorias/views.py
+++ tutorias/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required # Para requerir que el usuario esté autenticado
 from users.models import Usuario
 from .models import Tutoria
 from .forms import CreaNuevaTutoria
@@ -10,12 +11,14 @@ def tutorias(request, id):
         'listatutorias' : listatutorias,
     })
 
+@login_required(login_url='core:home')
 def detalle_tutoria(request, tutoria_id):
     tutoria = get_object_or_404(Tutoria, id = tutoria_id)
     return render(request, 'detalleTutoria.html', {
         'tutoria':tutoria,
     })
 
+@login_required(login_url='core:home')
 def crear_tutoria(request):
     if request.method == 'GET':
         return render(request, 'crearTutoria.html', {
@@ -25,6 +28,7 @@ def crear_tutoria(request):
         Tutoria.objects.create(Nombre=request.POST["Nombre"],Tema=request.POST["Tema"],tutor=get_object_or_404(Usuario, id = 1), usuario=get_object_or_404(Usuario, id = 2))
         return redirect('tutorias',2)
 
+@login_required(login_url='core:home')
 def eliminar_tutoria(request): #falta
     if request.method == 'GET':
         return render(request, 'crearTutoria.html', {
--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, get_user_model, login, logout # Para autenticar, crear usuarios y cerrar sesión
 from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password # Para encriptar la contraseña
+from django.http.response import HttpResponse, JsonResponse # Para devolver respuestas
 
 def user_login(request):
     if request.method == 'POST':
@@ -13,3 +15,29 @@ def user_login(request):
             # Mensaje de error si las credenciales son incorrectas
             return render(request, 'users/login.html', {'error_message': 'Nombre de usuario o contraseña incorrecta'})
     return render(request, 'users/login.html')
+
+def user_singup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        name = request.POST['name']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        try:
+            user = get_user_model().objects.create(
+                email=username,
+                nombre=name,
+                password=make_password(password),
+                is_active=True
+            )
+            login(request, user)
+            return redirect('core:home')
+
+        except Exception as e:
+            print(e)
+            return JsonResponse({'detail': f'{e}'})
+    return render(request, 'users/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('core:home')
