88dc114d34c4aafc17e0c59f1b35c64d709502a3
federicoGuerrerot <fguerrerot@unal.edu.co>
2023-03-27 08:40:23 -0500

    solucion de errores front

--- tutorias/views.py
+++ tutorias/views.py
@@ -38,7 +38,7 @@ def solicitarTutoria(request, emailtutor):
         #if form.is_valid():
             #new_tutoria = form.save(commit=False)
             #form.save_m2m()
-        #### ejemplo para seguir con usuario, para tutoria es solo: new_tutoria = form.save()
+        #### ejemplo para seguir con usuario, para tutoria es solo: new_tutoria = form.save().
 
         Tutoria.objects.create(nombre=request.POST["Nombre"],tema=request.POST["Tema"],tutor=tutor, usuario=estudiante)
         return redirect('tutorias')
