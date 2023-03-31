d0e2a789af3ae69ca9ce4859693d9914828ee231
JuanOspina05 <juospinae@unal.edu.co>
2023-03-31 01:17:15 -0500

    se crea vista de mi perfil

--- perfiles/views.py
+++ perfiles/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PerfilForm
 from users.models import Usuario
-from django.contrib.auth.decorators import login_required
-# Create your views here.
+
+def mi_perfil(request):
+    if not request.user.is_authenticated:
+        return redirect('users:login')
+
+    if request.method == "POST":
+        form = PerfilForm(request.POST, request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('perfiles:mi_perfil')
+    else:
+        form = PerfilForm(instance=request.user)
+    return render(request, 'perfiles/mi_perfil.html', {'form': form})
