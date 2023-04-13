1c6dc9833422f17740a4e6d46de16b9ee1b5cdc9
JuanOspina05 <juospinae@unal.edu.co>
2023-04-13 02:15:36 -0500

    Se añade casilla de tutor en perfil

--- perfiles/forms.py
+++ perfiles/forms.py
@@ -4,4 +4,4 @@ from users.models import Usuario
 class PerfilForm(forms.ModelForm):
     class Meta:
         model = Usuario
-        fields = ('nombre', 'celular', 'descripcion', 'foto_perfil')
+        fields = ('nombre', 'celular', 'descripcion', 'foto_perfil','is_tutor' , 'tags')
