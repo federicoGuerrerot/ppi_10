a335455ac5d3bf16ec67c92931a70718c2bb0dab
JuanOspina05 <juospinae@unal.edu.co>
2023-04-14 08:46:50 -0500

    Ajustes en perfil para el tutor

--- perfiles/forms.py
+++ perfiles/forms.py
@@ -4,4 +4,4 @@ from users.models import Usuario
 class PerfilForm(forms.ModelForm):
     class Meta:
         model = Usuario
-        fields = ('nombre', 'celular', 'descripcion', 'foto_perfil','is_tutor' , 'tags')
+        fields = ('nombre', 'celular', 'descripcion', 'foto_perfil','is_tutor' , 'tags', 'tarifa')
