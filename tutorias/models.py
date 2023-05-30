06074697f3825cce1eb7200c43893705c04e40de
federicoGuerrerot <fguerrerot@unal.edu.co>
2023-05-29 23:10:31 -0500

    primeros ajustes para apicalendar

--- tutorias/models.py
+++ tutorias/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.conf import settings
+from google_auth_oauthlib.flow import InstalledAppFlow
+import pickle
+import datetime
+from datetime import timedelta
+from googleapiclient.discovery import build
 import uuid #para crear id unicos, y que sirvan a la hora de migrar la base de datos, si lo vemos necesario.
 
 # Create your models here.
@@ -23,3 +28,36 @@ class Tutoria(models.Model):
     def __str__(self):
         """Devuelve el nombre de la tutoria junto a su tema"""
         return "Tutoria: " + self.nombre + ". (" + self.tema + ")"
+    
+    def addCalendario(self):
+        """Agrega la tutoria al calendario"""
+        
+        start_time = datetime.datetime.strptime(self.fecha,"%Y-%m-%d %H:%M:%S")
+        end_time= start_time + timedelta(minutes=120)
+
+        scopes = ['https://www.googleapis.com/auth/calendar']
+        credentials = pickle.load(open('H:\\django projects\\token.pkl', 'rb'))
+        service = build('calendar', 'v3', credentials=credentials)
+
+        event = (
+            service.events()
+            .insert(
+                calendarId="primary",
+                body={
+                    "summary": self.nombre,
+                
+                    "start": {"dateTime": start_time.isoformat(),
+                    'timeZone': 'America/Bogota',
+                
+                    
+                    },
+                    "end": {
+                        "dateTime": end_time.isoformat(),
+                        'timeZone': 'America/Bogota',
+                    },
+                    "attendees":[{"email":self.usuario.email}
+                    
+                    ]
+                },
+            ).execute()
+        )
