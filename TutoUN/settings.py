30cb9d89034fa9bc3478e0c4892c0afc22655ddc
federicoGuerrerot <fguerrerot@unal.edu.co>
2023-06-02 09:38:12 -0500

    host despliegue

--- TutoUN/settings.py
+++ TutoUN/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ebjp=ge1e^)qk9$3w0@nykqm^^h0iexf^al5(220mlm*cn_d2t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fguerrerot.pythonanywhere.com']
 
 
 # Application definition
